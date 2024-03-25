7080a1513927eb063b645d2c012dbf40169afbae
shrewdninja <varun.sundaram21@gmail.com>
2024-03-25 02:00:08 -0300

    changes in base and search page

--- eCom-master/core/forms.py
+++ eCom-master/core/forms.py
@@ -17,18 +17,18 @@ class SignupForm(UserCreationForm):
          model=User
          fields=('username', 'email', 'password1', 'password2')
     username=forms.CharField(widget=forms.TextInput(attrs={
-         'placeholder': 'Enter username',
+         'placeholder': 'Username',
          'class': 'w-full py-4 px-6 rounded-xl'
     }))
     email=forms.CharField(widget=forms.EmailInput(attrs={
-         'placeholder': 'Enter email address',
+         'placeholder': 'Email address',
          'class': 'w-full py-4 px-6 rounded-xl'
     }))
     password1=forms.CharField(widget=forms.PasswordInput(attrs={
-         'placeholder': 'Enter password',
+         'placeholder': 'Password',
          'class': 'w-full py-4 px-6 rounded-xl'
     }))
     password2=forms.CharField(widget=forms.PasswordInput(attrs={
-         'placeholder': 'Repeat password',
+         'placeholder': 'Re-enter Password',
          'class': 'w-full py-4 px-6 rounded-xl'
     }))
