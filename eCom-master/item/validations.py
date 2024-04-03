2fe235b6890a0fb4c443bc198ecd99a072918ff4
shrewdninja <varun.sundaram21@gmail.com>
2024-04-03 14:35:00 -0300

    added more validations

--- eCom-master/item/validations.py
+++ eCom-master/item/validations.py
@@ -19,26 +19,25 @@ def validate_username(value):
         raise ValidationError(u'%s violates constraints. Please verify and enter again' % value)
     
 def validate_description(value):
-    pattern = r'^(.|\s)+[\w\.]+(.|\s)$'
+    pattern = r'^[a-zA-Z \.\,\-\s]{3,50}$'
     if not re.match(pattern, value):
         raise ValidationError(u'%s violates constraints. Please verify and enter again' % value)
     
 def validate_card(value):
-    if value.isdigit()==False or len(value)!=16:
+    if value.isdigit()==False or len(value)!=16 or int(value)==0:
         raise ValidationError(u'%s violates constraints. Please verify and enter again' % value)
     
 def validate_cvv(value):
-    if value.isdigit()==False or len(value)!=3:
+    if value.isdigit()==False or len(value)!=3 or int(value)==0:
         raise ValidationError(u'%s violates constraints. Please verify and enter again' % value)
     
 def validate_date(value):
     if value[0:4].isdigit()==False or value[4]!='/' or value[5:7].isdigit()==False or len(value)!=7 or int(value[5:7])>12 or int(value[5:7])==0:
         raise ValidationError(u'%s violates constraints. Please verify and enter again' % value)
-    else:
-        if(int(value[0:4])<datetime.now().year):
-            raise ValidationError("Card has expired. Please enter a different card.")
-        elif(int(value[0:4])==datetime.now().year and int(value[5:7])<datetime.now().month):
-            raise ValidationError("Card has expired. Please enter a different card.")
+    elif(int(value[0:4])<datetime.now().year):
+        raise ValidationError("Card has expired. Please enter a different card.")
+    elif(int(value[0:4])==datetime.now().year and int(value[5:7])<datetime.now().month):
+        raise ValidationError("Card has expired. Please enter a different card.")
 
 def validate_price(value):
     if float(value)<0 or float(value)>999999:
